317232dd3bcaf6b4ac48cd14dcd37ebe8184c20d
Fauzan Rafi Sidiq <fauzanrafisidiq@gmail.com>
2019-09-15 11:41:21 +0700

    Adding det, transpose, adjoint, minor(i, j), and cofactor

--- Matriks.java
+++ Matriks.java
@@ -10,8 +10,10 @@ class Matriks{
 	// BERBENTUK AUGMENTED MATRIKS
 
 	// Metode
-	Matriks(){
-
+	Matriks(int rowEff, int colEff){
+		this.NeffBar = rowEff;
+        this.NeffKol = colEff;
+        angka = new float[rowEff+1][colEff+1];
 	}
 
 	void InputDataMat(){
@@ -84,4 +86,82 @@ class Matriks{
 		// DEBUG: NANTI DIBUANG AJA
 		System.out.println("R" + a + " <- R" + a + " + R" + b + "*" + k);
 	}
+
+	public Matriks minorMatriks(int idxRow, int idxCol){
+        Matriks holder = new Matriks(this.NeffBar-1, this.NeffKol-1);
+        for (int i = 1; i <= holder.NeffBar; i++){
+            for (int j = 1; j <= holder.NeffKol; j++){
+                if(i >= idxRow){
+                    if(j >= idxCol){
+                        holder.angka[i][j] = this.angka[i+1][j+1];
+                    } else {
+                        holder.angka[i][j] = this.angka[i+1][j];
+                    }
+                } else {
+                    if(j >= idxCol){
+                        holder.angka[i][j] = this.angka[i][j+1];
+                    } else {
+                        holder.angka[i][j] = this.angka[i][j];
+                    }
+                }
+            }
+        }
+        return holder;
+    }
+
+	public float determinan(){
+        float sum = 0f;
+        int multiplier;
+        Matriks holder = new Matriks(this.NeffBar-1, this.NeffKol-1);
+        if(this.NeffBar == 2){ // BASIS saat 2x2
+            return this.angka[1][1] * this.angka[2][2] - this.angka[1][2] * this.angka[2][1];
+        } else {
+            for (int i = 1; i <= this.NeffBar; i++){
+                holder = this.minorMatriks(i, 1);
+                if ((i + 1) % 2 == 0){ 
+                    multiplier = 1;
+                } else {
+                    multiplier = -1;
+                }
+                sum += this.angka[i][1] * multiplier * holder.determinan(); //RECC
+            }
+        }
+        return sum;
+    }
+
+	public Matriks kofaktor(){
+        int multiplier;
+        Matriks MatriksKofaktor = new Matriks(this.NeffBar, this.NeffKol);
+        for (int i = 1; i <= this.NeffBar; i++){
+            for (int j = 1; j <= this.NeffKol; j++){    
+                if((i + j) % 2 == 0){
+                    multiplier = 1;
+                } else {
+                    multiplier = -1;
+                }
+                
+                if(this.minorMatriks(i, j).determinan() != 0){
+                     MatriksKofaktor.angka[i][j] = multiplier * this.minorMatriks(i, j).determinan();
+                } else {
+                    MatriksKofaktor.angka[i][j] = 0;
+                }
+                
+            }
+        }
+        return MatriksKofaktor;
+    }
+
+    public Matriks transpose(){
+        Matriks T = new Matriks(this.NeffBar, this.NeffKol);
+        for (int i = 1; i <= this.NeffBar; i++){
+            for (int j = 1; j <= this.NeffKol; j++){
+                T.angka[j][i] = this.angka[i][j];       
+            }
+        }
+        return T;
+    }
+
+    public Matriks adjoin(){
+        return this.kofaktor().transpose();
+    }
 }
